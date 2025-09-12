e0ffc222293243ecabf202fa67287c987125d395
XIVN1987 <XIVN1987@163.com>
2025-09-12 17:05:32 +0800

    replace OrderedDict with dict

--- device/__init__.py
+++ device/__init__.py
@@ -1,10 +1,8 @@
-import collections
-
 from . import MT7687
 from . import STM32F103_LS
 from . import STM32F405_LS
 
 
-Devices = collections.OrderedDict([
-    ('MT7687',         MT7687.MT7687),
-])
+Devices = {
+    'MT7687':       MT7687.MT7687,
+}
